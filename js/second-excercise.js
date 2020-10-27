60bd16318589719935b336301e694f32ea062287
unknown <vpupkin@company.com>
2020-10-27 16:03:14 +0200

    Add third excercise and fixed mistake

--- js/second-excercise.js
+++ js/second-excercise.js
@@ -1,7 +1,3 @@
-// Перепиши функцию toggleUserState() так, чтобы она не использовала 
-// callback - функцию callback, а принимала всего два параметра
-// allUsers и userName и возвращала промис.
-
 const users = [
   { name: 'Mango', active: true },
   { name: 'Poly', active: false },
@@ -19,14 +15,5 @@ const toggleUserState = (allUsers, userName) => {
 
 const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-// toggleUserState(users, 'Mango', logger);
-// toggleUserState(users, 'Lux', logger);
-
-/*
- * Должно работать так
- */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
